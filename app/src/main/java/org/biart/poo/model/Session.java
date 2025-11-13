2442893d0244d702017a7a2f1a34a5b9d9d7ee57
bibig <91799545+biankagouvea@users.noreply.github.com>
2025-11-13 12:20:56 +0100

    .

--- app/src/main/java/org/biart/poo/model/Session.java
+++ app/src/main/java/org/biart/poo/model/Session.java
@@ -2,16 +2,17 @@ package org.biart.poo.model;
 import java.time.LocalDate;
 import java.util.List;
 import org.biart.poo.state.SessionState;
+import org.biart.poo.observer.Observable;;
 
-public class Session {
+public class Session implements Observable {
 
-    String id;
-    String courseId;
-    LocalDate startAt;
-    LocalDate endAt;
-    int maxPlaces;
-    SessionState state;
-    List<Student> students;
+    private String id;
+    private String courseId;
+    private LocalDate startAt;
+    private LocalDate endAt;
+    private int maxPlaces;
+    private SessionState state;
+    private List<Student> students;
 
     public void subscribe(Student student) {
         if (students.size() < maxPlaces) {
@@ -25,6 +26,24 @@ public class Session {
 
     public void setState(SessionState state) {
         this.state = state;
+    }
+
+    @Override
+    public void addObservable(Observable Observable) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'addObservable'");
+    }
+
+    @Override
+    public void removeObservable(Observable Observable) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeObservable'");
+    }
+
+    @Override
+    public void notifyObservable(Object arg) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'notifyObservable'");
     }
 
 }
--- app/src/main/java/org/biart/poo/model/Teacher.java
+++ app/src/main/java/org/biart/poo/model/Teacher.java
@@ -1,5 +1,8 @@
 package org.biart.poo.model;
+import java.util.List;
 
 public class Teacher {
-
+    private String id;                    
+    private String name;                   
+    private List<String> specialites; 
 }
