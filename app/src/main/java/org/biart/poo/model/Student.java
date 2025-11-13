aadc3de8e0e002d2b7983ee54e9b1eac2966597f
bibig <91799545+biankagouvea@users.noreply.github.com>
2025-11-13 12:00:39 +0100

    student

--- app/src/main/java/org/biart/poo/model/Student.java
+++ app/src/main/java/org/biart/poo/model/Student.java
@@ -1,5 +1,12 @@
 package org.biart.poo.model;
 
 public class Student {
+    String id;
+    String name;    
+    String email;
+
+    public void subscribe(Session session) {
+        session.subscribe(this);
+    }
 
 }
